d667ca0418359f14f641267330cc4c3d120213dd
juancedeno96 <cedenojj7@gmail.com>
2021-01-20 15:09:03 -0700

    created test for React components

---
+++ src/__tests__/Post.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, act } from "@testing-library/react";
+import Post from "../views/Post";
+import axios from "axios";
+import {MemoryRouter} from "react-router";
+import { posts } from "./__data__/testData";
+
+it("Post component will render a post", async () => {
+  const post = posts[0];
+  let container;
+  jest
+    .spyOn(axios, "get")
+    .mockImplementation(() => Promise.resolve({ data: post }));
+
+  await act(async () => {
+    let renderObj = render(
+      <MemoryRouter>
+        <Post match={{ params: { postIt: 1 } }} />
+      </MemoryRouter>
+    );
+    container = renderObj.container;
+  });
+
+  expect(container.textContent).toContain(post.text);
+});
